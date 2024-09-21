34723c2a189b28aca1f88c001ceca691b16d6313
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-09-21 18:01:51 +0800

    Assignment Update

--- Assets/Scripts/Week4 Codes/Collisioner.cs
+++ Assets/Scripts/Week4 Codes/Collisioner.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 
 public class Collisioner : MonoBehaviour
 {
-    [SerializeField] private List<Rigidbody> elastic, inelastic, superInelastic;
+    [SerializeField] private List<Rigidbody> elastic, inelastic, perfectlyInelastic;
+    [SerializeField] private float box1_initialVelocity, box2_initialVelocity;
 
     [Min(0.0001f)]
-    [SerializeField] private float box1_initialVelocity, box2_initialVelocity, box1_mass, box2_mass;
+    [SerializeField] private float box1_mass, box2_mass;
 
-    private void Start()
+    [Button]
+    public void StartDemo()
     {
             elastic[0].velocity = new Vector3(box1_initialVelocity, 0, 0);
             inelastic[0].velocity = new Vector3(box1_initialVelocity, 0, 0);
-            superInelastic[0].velocity = new Vector3(box1_initialVelocity, 0, 0);
+            perfectlyInelastic[0].velocity = new Vector3(box1_initialVelocity, 0, 0);
 
             elastic[1].velocity = new Vector3(box2_initialVelocity, 0, 0);
             inelastic[1].velocity = new Vector3(box2_initialVelocity, 0, 0);
-            superInelastic[1].velocity = new Vector3(box2_initialVelocity, 0, 0);
+            perfectlyInelastic[1].velocity = new Vector3(box2_initialVelocity, 0, 0);
 
             elastic[0].mass = box1_mass;
             inelastic[0].mass = box1_mass;
-            superInelastic[0].mass = box1_mass;
+            perfectlyInelastic[0].mass = box1_mass;
 
             elastic[1].mass = box2_mass;
             inelastic[1].mass = box2_mass;
-            superInelastic[1].mass = box2_mass;
+            perfectlyInelastic[1].mass = box2_mass;
     }
 }
